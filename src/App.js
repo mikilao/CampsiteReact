16cfd1c2630adf185d4800ddec4741bd6ac8bd95
Rachel <thelivedish@gmail.com>
2020-04-22 22:56:20 -0700

    week 2 react app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Directory from './components/DirectoryComponents';
+import './App.css';
+import { CAMPSITES } from './shared/campsites';
+
+class App extends Component {
+    constructor(props){
+        super( props);
+        this.state={
+            campsites: CAMPSITES
+        };
+    }
+  render() {
+      return (
+          <div className="App">
+              <Navbar dark color="primary">
+              <div className="container">
+              <NavbarBrand href="/">NuCamp</NavbarBrand>
+                    <Directory campsites ={this.state.campsites}/>
+              
+              </div>
+              </Navbar>
+          </div>
+      );
+  }
+}
+
+
+export default App;
